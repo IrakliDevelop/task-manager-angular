853613b8c6916fa253e4bb85cc3b8af9de7a7dbb
Irakli Iremashvili <i_iremashvili@cu.edu.ge>
2021-03-14 20:39:14 +0400

    implement drag and drop

---
+++ src/app/models/column.model.ts
@@ -0,0 +1,7 @@
+export class Column {
+  constructor(
+    public name: string,
+    public tasks: string[]
+  ) {
+  }
+}
--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {Board} from '../../models/board.model';
+import {Column} from '../../models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  constructor() { }
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Some random idea',
+      'This is another random idea'
+    ]),
+    new Column('Research', [
+      ' Lorem ipsum',
+      'Fap',
+      'Thsi was in the \'Research Column\''
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
+
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep',
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 
 }
